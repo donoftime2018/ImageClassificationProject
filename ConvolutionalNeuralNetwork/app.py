c00f0111f55220e4fdf6c4d2b3ae35c5c955b288
donoftime2018 <donoftime2018@gmail.com>
2025-09-23 08:42:34 -0400

    WSGIMiddleware wrapper

--- ConvolutionalNeuralNetwork/app.py
+++ ConvolutionalNeuralNetwork/app.py
@@ -1,8 +1,10 @@
 from flask import Flask
-from asgi_lifespan import LifespanManager
+from uvicorn.middleware.wsgi import WSGIMiddleware
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "Hello, World!"
+
+app = WSGIMiddleware(app)
