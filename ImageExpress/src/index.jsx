c1df66c1655446aaf91b72a94f7d715967aca0d3
donoftime2018 <donoftime2018@gmail.com>
2025-10-06 19:39:39 -0400

    setting up Router

--- ImageExpress/src/index.jsx
+++ ImageExpress/src/index.jsx
@@ -2,7 +2,10 @@
 import { render } from 'solid-js/web'
 import './index.css'
 import App from './App.jsx'
+import {Router, Route} from '@solidjs/router'
 
 const root = document.getElementById('root')
 
-render(() => <App />, root)
+render(() => <Router root={App} />,
+    
+root)
