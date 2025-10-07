7275706315ab0b2ef86c47d4d237476f70d72e16
donoftime2018 <donoftime2018@gmail.com>
2025-10-06 21:59:08 -0400

    using props.children in App.jsx instead of Outlet

--- ImageExpress/src/App.jsx
+++ ImageExpress/src/App.jsx
@@ -2,12 +2,12 @@ import { createSignal } from 'solid-js'
 import solidLogo from './assets/solid.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import {Outlet} from '@solidjs/router'
+
 
 function App(props) {
   return (
     <>
-      <Outlet/>
+      {props.children}
     </>
   )
 }
