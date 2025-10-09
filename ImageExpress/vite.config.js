18f71b76e56c6e6c48eb61aae82cbf3eef811603
donoftime2018 <donoftime2018@gmail.com>
2025-10-09 15:16:32 -0400

    adding cache busting into the base; if production use ImageClassificationProject otherwise /

--- ImageExpress/vite.config.js
+++ ImageExpress/vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
 
 export default defineConfig({
-  // base: "/ImageClassificationProject/",
+  base: process.env.NODE_ENV === 'production' 
+    ? `/ImageClassificationProject/?v=${Date.now()}/`
+    : '/',
   plugins: [solid()],
   build: {
     target: 'esnext'
