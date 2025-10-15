51cc8b3e0d93bfbc305a386ce0685e34487ca77f
donoftime2018 <donoftime2018@gmail.com>
2025-10-15 19:19:13 -0400

    commencing work on top navbar for homepage

--- ImageExpress/src/pages/Home/Home.jsx
+++ ImageExpress/src/pages/Home/Home.jsx
@@ -1,11 +1,11 @@
 import './Home.css';
+import Navbar from '../Navbar/Navbar';
 
 export default function Home(){
   console.log("Home component loaded");
   return (
-    <div>
-      <h1>Home</h1>
-      <p>Welcome to ImageExpress!</p>
-    </div>
+    <>
+    <Navbar />
+    </>
   );
 }
---
+++ ImageExpress/src/pages/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+export default function Navbar(){
+
+    return(<>
+      <nav className="bg-gray-800 p-4 fixed top-0 inset-x-0 w-full">
+
+        <div className="container mx-auto flex justify-between items-center">
+          <div className="text-white font-bold text-xl">
+            ImageExpress
+          </div>
+          <div>
+            
+          </div>
+        </div>
+      </nav>
+    </>)
+}
