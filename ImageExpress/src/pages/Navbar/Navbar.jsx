d8b7e783a4fe055635ffc3b356c46b182371c3d1
donoftime2018 <donoftime2018@gmail.com>
2025-10-15 19:22:30 -0400

    making navbar color same as in Figma prototype

--- ImageExpress/src/pages/Navbar/Navbar.jsx
+++ ImageExpress/src/pages/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 export default function Navbar(){
 
     return(<>
-      <nav className="bg-gray-800 p-4 fixed top-0 inset-x-0 w-full">
+      <nav className="bg-[#F5F5F5] p-4 fixed top-0 inset-x-0 w-full">
 
         <div className="container mx-auto flex justify-between items-center">
           <div className="text-white font-bold text-xl">
