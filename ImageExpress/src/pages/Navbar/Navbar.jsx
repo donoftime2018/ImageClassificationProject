d0e53152d9726216e7039e5d58ef1bda60d9b9c1
donoftime2018 <donoftime2018@gmail.com>
2025-10-15 19:30:19 -0400

    changed background color of navbar plus put images in subdir in public dir

--- ImageExpress/src/pages/Navbar/Navbar.jsx
+++ ImageExpress/src/pages/Navbar/Navbar.jsx
@@ -1,15 +1,17 @@
+
+
 export default function Navbar(){
 
+
     return(<>
-      <nav className="bg-[#F5F5F5] p-4 fixed top-0 inset-x-0 w-full">
+      <nav className="bg-[#D9D9D9] p-4 fixed top-0 inset-x-0 w-full">
 
         <div className="container mx-auto flex justify-between items-center">
-          <div className="text-white font-bold text-xl">
-            ImageExpress
-          </div>
-          <div>
-            
-          </div>
+            <ul>
+              <li>
+                <img src="/images/ImageExpresslogo.png" alt="Logo"/>
+              </li>
+            </ul>
         </div>
       </nav>
     </>)
