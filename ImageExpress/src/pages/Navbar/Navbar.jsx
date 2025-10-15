e919486abc3c0ccd111f56a5887cb6a527f2a44b
donoftime2018 <donoftime2018@gmail.com>
2025-10-15 19:34:02 -0400

    adding welcome for fun to top navbar

--- ImageExpress/src/pages/Navbar/Navbar.jsx
+++ ImageExpress/src/pages/Navbar/Navbar.jsx
@@ -6,10 +6,16 @@ export default function Navbar(){
     return(<>
       <nav className="bg-[#D9D9D9] p-4 fixed top-0 inset-x-0 w-full">
 
-        <div className="container mx-auto flex justify-between items-center">
+        <div className="container mx-auto flex justify-between items-center text-black font-extrabold">
             <ul>
               <li>
                 <img src="/images/ImageExpresslogo.png" alt="Logo"/>
+              </li>
+            </ul>
+
+            <ul>
+              <li className="text-[20px]">
+                Welcome For Fun
               </li>
             </ul>
         </div>
