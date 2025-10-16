51eda1d3e4c84fd20dc09ef7361f6cc55c6a54a9
donoftime2018 <donoftime2018@gmail.com>
2025-10-15 20:05:08 -0400

    adding button to navigate to about page in navbar

--- ImageExpress/src/pages/Navbar/Navbar.jsx
+++ ImageExpress/src/pages/Navbar/Navbar.jsx
@@ -4,9 +4,9 @@ export default function Navbar(){
 
 
     return(<>
-      <nav className="bg-[#D9D9D9] p-4 fixed top-0 inset-x-0 w-full">
+      <nav className="bg-[#D9D9D9] p-4 fixed top-0 inset-x-0 w-full text-black font-extrabold">
 
-        <div className="container mx-auto flex justify-between items-center text-black font-extrabold">
+        <div className="container mx-auto flex justify-between items-center">
             <ul>
               <li>
                 <img src="/images/ImageExpresslogo.png" alt="Logo"/>
@@ -14,8 +14,16 @@ export default function Navbar(){
             </ul>
 
             <ul>
-              <li className="text-[20px]">
+              <li className="text-[22px]">
                 Welcome For Fun
+              </li>
+            </ul>
+
+            <ul>
+              <li>
+                <button class="bg-[#E8F0FF] text-[20px] rounded-[18px]">
+                  <span class="font-bold">About</span>
+                </button>
               </li>
             </ul>
         </div>
