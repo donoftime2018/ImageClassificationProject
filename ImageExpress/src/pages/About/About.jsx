2939c89e940420a5bc982d1ac7ebec097a0a4fb2
donoftime2018 <donoftime2018@gmail.com>
2025-10-16 16:17:07 -0400

    Starting about page

--- ImageExpress/src/pages/About/About.jsx
+++ ImageExpress/src/pages/About/About.jsx
@@ -1,4 +1,19 @@
+import "./About.css";
+
 export default function About(){
     console.log("About component loaded");
-    return (<div>ABOUT</div>)
+    return (
+        <div className="about-container">
+            <div className="font-[700] text-3xl font-[poppins]">
+                <p>About Image Express</p>
+            </div>
+
+            <div className="font-[poppins] mr-0 ml-0 pr-0 pl-0">
+                <p>Image Express is a modern, user-friendly web application designed to classify images
+                using a custom-trained Convolutional Neural Network (CNN). It distinguishes between
+                characters from two fictional universes: Pokémon and Thomas the Tank Engine. The app
+                uses a Solid.JS front-end and a Python backend.</p>
+            </div>
+        </div>
+)
 }
