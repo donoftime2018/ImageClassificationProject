850dd7b5dd16c3ce81c23c60fdde0e116bdb92c8
donoftime2018 <donoftime2018@gmail.com>
2025-10-17 02:01:08 -0400

    working on results page

--- ImageExpress/src/pages/Results/Results.jsx
+++ ImageExpress/src/pages/Results/Results.jsx
@@ -1,3 +1,15 @@
+import "./Results.css";
 export default function Results(){
-    return (<div>RESULTS</div>)
+    return (
+        <div className="resultsContainer">
+            <div className="text-3xl font-[poppins] ">
+                <p>Classification Result</p>
+            </div>
+
+            <div className=" mt-24 w-128 h-56 bg-white stroke-[#Cbd5e1] stroke-1 rounded-lg drop-shadow-md/20">
+                <p className="mr-45 mt-7 font-[poppins]"><span className="font-bold">Prediction</span></p>
+                
+            </div>
+        </div>
+)
 }
