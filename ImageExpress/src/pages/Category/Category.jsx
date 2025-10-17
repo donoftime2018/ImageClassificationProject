b7cbeba6a8ab8891fa6563facda917a733c95498
donoftime2018 <donoftime2018@gmail.com>
2025-10-16 21:54:53 -0400

    Finished Category page

--- ImageExpress/src/pages/Category/Category.jsx
+++ ImageExpress/src/pages/Category/Category.jsx
@@ -9,10 +9,16 @@ export default function Category(){
                 <p className="font-extrabold">Select a Category</p>
             </div>
 
-            <div class="flex flex-row justify-center space-x-45 mt-20">
+            <div class="flex flex-row justify-center space-x-45 mt-29">
                 <img src="public/images/Group 13.png" alt="Pokemon" className="categoryImage"/>
                 <img src="public/images/Group 12.png" alt="Thomas the Tank Engine" className="categoryImage"/>
             </div>
+
+             <div className="mt-35 font-[poppins]">
+                <button type='button' class="!bg-[#2B6DE0] py-2 px-4 text-white">Next: Upload →</button>
+            </div>
+
+
         </div>
 
 
