1ab234022ba445b1740446ba966e63ec716e218c
donoftime2018 <donoftime2018@gmail.com>
2025-10-16 23:44:19 -0400

    working on uploading image page

--- ImageExpress/src/pages/ImageUpload/ImageUpload.jsx
+++ ImageExpress/src/pages/ImageUpload/ImageUpload.jsx
@@ -1,5 +1,16 @@
+import "./ImageUpload.css";
 export default function ImageUpload(){
     console.log("Image upload component loaded");
 
-    return (<div>IMAGE UPLOAD</div>)
+    return (
+    <div className="uploadContainer">
+        <div className="text-3xl font-[poppins]">
+            <p className="font-extrabold">Upload an Image</p>
+        </div>
+
+        <div class="mt-10 bg-white stroke-[#CBD5E1] rounded-md drop-shadow-xl/50">
+    BANJo-KAzooIE
+        </div>
+    </div>
+    )
 }
