149e1cc9bdc06550d7496b7bdaee9b8522bd3128
donoftime2018 <donoftime2018@gmail.com>
2025-10-16 20:03:31 -0400

    image express site info matches Figma in terms of lines/word placement

--- ImageExpress/src/pages/About/About.jsx
+++ ImageExpress/src/pages/About/About.jsx
@@ -4,12 +4,12 @@ export default function About(){
     console.log("About component loaded");
     return (
         <div className="about-container">
-            <div className="font-[700] text-3xl font-[poppins]">
+            <div className="font-[700] text-3xl font-[poppins] mb-15">
                 <p>About Image Express</p>
             </div>
 
-            <div className="font-[poppins] mr-0 ml-0 pr-0 pl-0">
-                <p>Image Express is a modern, user-friendly web application designed to classify images
+            <div className="font-[poppins]">
+                <p className="w-175">Image Express is a modern, user-friendly web application designed to classify images
                 using a custom-trained Convolutional Neural Network (CNN). It distinguishes between
                 characters from two fictional universes: Pokémon and Thomas the Tank Engine. The app
                 uses a Solid.JS front-end and a Python backend.</p>
