db3cc2c1ec60d12a5b4f57c89bce67a0ddce8008
donoftime2018 <donoftime2018@gmail.com>
2025-10-20 01:44:36 -0400

    adding buttons to Results page

--- ImageExpress/src/pages/Results/Results.jsx
+++ ImageExpress/src/pages/Results/Results.jsx
@@ -1,5 +1,16 @@
 import "./Results.css";
+import {useNavigate} from "@solidjs/router"
 export default function Results(){
+    const navigate = useNavigate();
+
+    function goToImageUpload(){
+        navigate("/imageUpload");
+    }
+
+    function goToHomePage(){
+        navigate("/");
+    }
+
     return (
         <div className="resultsContainer">
             <div className="text-3xl font-[poppins] ">
@@ -7,12 +18,18 @@ export default function Results(){
             </div>
 
             <div className=" mt-24 w-128 h-56 bg-white stroke-[#Cbd5e1] stroke-1 rounded-lg drop-shadow-md/20">
-                <div className="mr-55 flex flex-col justify-items-center align-start">
-                    <p className="mt-7 font-[poppins]">Prediction</p>
-                    <p className="mt-4 font-[poppins]">Category</p>
-                    <p className="mt-4 font-[poppins]">Confidence</p>
+                <div className="flex flex-col">
+                    <p className="mr-55 mt-7 font-[poppins] font-bold">Prediction</p>
+                    <p className="mr-55 mt-4 font-[inter]">Category</p>
+                    <p className="mr-55 mt-3 font-[inter]">Confidence</p>
                 </div>
-            
+
+                <div className="mt-7 flex flex-row space-x-8 font-[inter]">
+                    <button onClick={goToImageUpload} type='button' class="!bg-[#2B6DE0] mt-5 py-2 px-4 text-white">Try Another</button>
+                    <button onClick={goToHomePage} type='button' class="!bg-[#D9D9D9] mt-5 py-2 px-4 text-black">
+                    <span className="font-bold">Home</span>
+                    </button>
+                </div>
                 
             </div>
 
