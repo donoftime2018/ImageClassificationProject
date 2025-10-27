b24a050fe608f2cbfe59ab95964e05ad74ab79be
donoftime2018 <donoftime2018@gmail.com>
2025-10-27 15:51:24 -0400

    Adds checkmark after category selected

--- ImageExpress/src/pages/Category/Category.jsx
+++ ImageExpress/src/pages/Category/Category.jsx
@@ -1,6 +1,7 @@
 import "./Category.css"
 import Group13 from "../../assets/Group 13.png"
 import Group12 from "../../assets/Group 12.png"
+import checkmark from "../../assets/checkmark.png"
 import {A, useNavigate} from "@solidjs/router"
 import { createSignal, onMount, onCleanup } from "solid-js"
 
@@ -15,7 +16,7 @@ export default function Category(){
     })
 
     onCleanup(()=>{
-        document.body.style.backgroundr = ""
+        document.body.style.background = ""
     })
 
     function goToImageUpload(){
@@ -26,6 +27,31 @@ export default function Category(){
             alert("Please select a category before proceeding.");
     }
 
+    function handleSelectCategory(selectedCategory){
+        if (selectedCategory === "thomas")
+        {
+            setCategory("thomas")
+            const element = document.getElementById("thomasSelector")
+            const rect = element.getBoundingClientRect();
+            console.log(rect);
+
+            var checkMark = document.createElement("img");
+            checkMark.setAttribute("src", checkmark);
+            checkMark.setAttribute("position", "absolute");
+            checkMark.setAttribute("width", "100px")
+            checkMark.setAttribute("height", "100px")
+            document.body.appendChild(checkMark);
+        }
+
+        else
+        {
+            setCategory("pokemon")
+            const element = document.getElementById("pokemonSelector")
+            const rect = element.getBoundingClientRect();
+            console.log(rect);
+        }
+    }
+
     return (
         <div className="categoryContainer">
             <div className="text-3xl font-[poppins]">
@@ -33,8 +59,8 @@ export default function Category(){
             </div>
 
             <div class="flex flex-row justify-center space-x-45 mt-29">
-                <img onClick={()=>{setCategory("pokemon")}} class="cursor-pointer" src={Group13} alt="Pokemon" className="categoryImage"/>
-                <img onClick={()=>{setCategory("thomas")}} class="cursor-pointer" src={Group12} alt="Thomas the Tank Engine" className="categoryImage"/>
+                <img id="pokemonSelector" onClick={()=>{handleSelectCategory("pokemon")}} class="cursor-pointer" src={Group13} alt="Pokemon" className="categoryImage"/>
+                <img id="thomasSelector" onClick={()=>{handleSelectCategory("thomas")}} class="cursor-pointer" src={Group12} alt="Thomas the Tank Engine" className="categoryImage"/>
             </div>
 
              <div className="mt-35 font-[poppins]">
