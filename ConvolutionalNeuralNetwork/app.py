d3d87caa83e92f0b73356df19fb4a9ffba02c646
donoftime2018 <donoftime2018@gmail.com>
2025-11-11 20:29:56 -0500

    ttte_model returns predictions for each class in json

--- ConvolutionalNeuralNetwork/app.py
+++ ConvolutionalNeuralNetwork/app.py
@@ -2,6 +2,8 @@ from flask import Flask, request, jsonify
 from PIL import Image
 from uvicorn.middleware.wsgi import WSGIMiddleware
 import numpy as np
+import tensorflow as tf
+from tensorflow import keras
 
 app = Flask(__name__)
 
@@ -16,12 +18,16 @@ def predict():
         return jsonify({"error": "No image received"}), 400
 
     img = Image.open(img_file.stream).convert('RGB')
-    img = img.resize((224, 224))
+    img = img.resize((180, 180))
     img_array = np.array(img) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
 
-    predicted_class = "Class name here!"  # Dummy response for now
+    ttte_model = tf.keras.models.load_model("ttte_model.keras")
+    predictions = ttte_model.predict(img_array)
+    # predicted_class = "Class name here!"  # Dummy response for now
 
-    return jsonify({"prediction": predicted_class})
+    return jsonify({"prediction": predictions.tolist()})
 
-app=WSGIMiddleware(app)
+if __name__ == '__main__':
+        app.run(debug=True)
+# app=WSGIMiddleware(app)
