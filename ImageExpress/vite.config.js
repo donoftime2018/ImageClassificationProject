0c3dbd55acfe764784c2ee4c325c16a5831d3caa
donoftime2018 <donoftime2018@gmail.com>
2025-11-15 23:21:01 -0500

    enable vite.config.js to allow .env variables

--- ImageExpress/vite.config.js
+++ ImageExpress/vite.config.js
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import solid from 'vite-plugin-solid'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
+export default ({mode}) => {
+  const env = loadEnv(mode, process.cwd(), '')
+return defineConfig({
   base: `/ImageClassificationProject/`,
   plugins: [solid(), tailwindcss()],
   build: {
     target: 'esnext'
+  },
+  define: {
+
   }
+  
 })
+
+}
