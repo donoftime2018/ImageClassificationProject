e3bb49e41cfeb0b39804c9120f1a6fb490fc2610
donoftime2018 <donoftime2018@gmail.com>
2025-11-29 23:39:50 -0500

    changing predictions based on universe specified by user

--- ConvolutionalNeuralNetwork/app.py
+++ ConvolutionalNeuralNetwork/app.py
@@ -16,21 +16,28 @@ def index():
 @app.route('/predict', methods=['POST'])
 def predict():
     img_file = request.files.get('image')
-    # universe = request.form.get('universe')
+    universe = request.form.get('universe')
     if img_file is None:
         return jsonify({"error": "No image received"}), 400
+    if universe is None:
+        return jsonify({"error": "No universe specified"}), 400
 
+    print(universe)
     img = Image.open(img_file.stream).convert('RGB')
     img = img.resize((180, 180))
     img_array = np.array(img) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
 
-    ttte_model = tf.keras.models.load_model("ttte_model.keras")
-    predictions = ttte_model.predict(img_array)
-    # predicted_class = "Class name here!"  # Dummy response for now
+    predictions = []
+    if universe == "thomas":
+        ttte_model = tf.keras.models.load_model("ttte_model.keras")
+        predictions = ttte_model.predict(img_array)
+        predictions = predictions.tolist()
+    else:
+        predictions = {"error": "Unknown universe"}
 
-    return jsonify({"prediction": predictions.tolist()})
+    return jsonify({"prediction": predictions})
 
-# if __name__ == '__main__':
-#         app.run(debug=True)
-app=WSGIMiddleware(app)
+if __name__ == '__main__':
+        app.run(debug=True)
+# app=WSGIMiddleware(app)
