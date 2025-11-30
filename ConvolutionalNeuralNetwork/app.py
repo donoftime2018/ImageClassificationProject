5b8fdf479f13c12c90a70755fa95ff44d09e07c5
donoftime2018 <donoftime2018@gmail.com>
2025-11-30 00:45:32 -0500

    predict json returns class associated with highest prediction

--- ConvolutionalNeuralNetwork/app.py
+++ ConvolutionalNeuralNetwork/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from PIL import Image
 from flask_cors import CORS
+import os
 from uvicorn.middleware.wsgi import WSGIMiddleware
 import numpy as np
 import tensorflow as tf
@@ -33,14 +34,17 @@ def predict():
         ttte_model = tf.keras.models.load_model("ttte_model.keras")
         predictions = ttte_model.predict(img_array)
         max_pred_index = np.argmax(predictions, axis=1)[0]
+        classes = os.listdir("../dataset/ttte/train")
+        print(classes)
         print(max_pred_index)
         predictions = predictions.tolist()
         print(predictions)
         most_accurate_pred = predictions[0][max_pred_index]
+        most_accurate_class = classes[max_pred_index]
     else:
         predictions = {"error": "Unknown universe"}
 
-    return jsonify({"prediction": most_accurate_pred})
+    return jsonify({"prediction": most_accurate_pred, "class": most_accurate_class})
 
 if __name__ == '__main__':
         app.run(debug=True)
