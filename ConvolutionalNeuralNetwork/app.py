c0ff0c73b15f587f8f2871bb74e76a67107a2cab
donoftime2018 <donoftime2018@gmail.com>
2025-12-06 18:10:14 -0500

    tflite classifier makes returns predictions correctly

--- ConvolutionalNeuralNetwork/app.py
+++ ConvolutionalNeuralNetwork/app.py
@@ -6,17 +6,18 @@ from uvicorn.middleware.wsgi import WSGIMiddleware
 import numpy as np
 import tensorflow as tf
 from huggingface_hub import hf_hub_download
-import keras
+import tf_keras
 
-os.environ["KERAS_BACKEND"] = "tensorflow"
+os.environ["KERAS_BACKEND"] = "jax"
 
 app = Flask(__name__)
 CORS(app)
 ttte_model_path = hf_hub_download(
      repo_id=os.getenv("REPO"),
-     filename="ttte_model.keras",
+     filename="ttte_classifier.tflite",
 )
-ttte_model = keras.models.load_model(ttte_model_path)
+tflite_interpreter_ttte = tf.lite.Interpreter(model_path=ttte_model_path)
+tflite_interpreter_ttte.allocate_tensors()
 
 @app.route('/')
 def index():
@@ -32,14 +33,25 @@ def predict():
         return jsonify({"error": "No universe specified"}), 400
 
     print(universe)
+
+
+
     img = Image.open(img_file.stream).convert('RGB')
     img = img.resize((180, 180))
-    img_array = np.array(img) / 255.0
+    img_array = np.array(img, dtype=np.float32) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
 
     predictions = []
     if universe == "thomas":
-        predictions = ttte_model.predict(img_array)
+        input_index = tflite_interpreter_ttte.get_input_details()[0]["index"]
+        output_index = tflite_interpreter_ttte.get_output_details()[0]["index"]
+
+        tflite_interpreter_ttte.set_tensor(input_index, img_array)
+        tflite_interpreter_ttte.invoke()
+
+        predictions = tflite_interpreter_ttte.get_tensor(output_index)
+        print(predictions)
+        # predictions = ttte_model.predict(img_array)
         max_pred_index = np.argmax(predictions, axis=1)[0]
         classes = sorted(os.listdir("../dataset/ttte/train"))
         print(classes)
@@ -53,6 +65,6 @@ def predict():
 
     return jsonify({"prediction": most_accurate_pred, "class": most_accurate_class})
 
-# if __name__ == '__main__':
-#         app.run(debug=True)
-app=WSGIMiddleware(app)
+if __name__ == '__main__':
+        app.run(debug=True)
+# app=WSGIMiddleware(app)
